0bef2f90bf8a9e840bdce8530dbfd54f1722402e
Bitan Sarkar <bitan.sarkar2017@gmail.com>
2022-09-12 15:18:43 +0530

    g

---
+++ Assignment_1_Bitan_Sarkar/prob_7/string_search.c
@@ -0,0 +1,8 @@
+// Write a C function mySubStr which takes two character strings s1, s2 and checks whether the
+// second string is a substring of the latter. If so, it should return the position (the index value)
+// of the occurrence of s2 in s1
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
